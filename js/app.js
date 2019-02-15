e3e5a6dc21aeed7e75824041311265e166ff9a53
2a-Ron <ematrix_frag@hotmail.com>
2019-02-14 23:53:49 -0800

    added app.js and started creating objects

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// create objects for pats stores.  note if the properties are to be updated they will eventually need to be variables..
+var pike = {
+	location: '1st and Pike',
+	minCust: '23', // per hour
+	maxCust: '65', // per hour
+	avgCookieSale: '6.3', //per hour
+	// add method to generate random number of customers per hour
+	// calc and store amounts of cookies purchased for each hour at each location using avgCookieSale and random num cust/hr
+	totalCookies: numTotalCookies
+};
+
+var seaTac = {
+	location: 'SeaTac Airport',
+	minCust: '3', // per hour
+	maxCust: '24', // per hour
+	avgCookieSale: '1.2', // per hour
+	// add method to generate random number of customers per hour
+	// calc and store amounts of cookies purchased for each hour at each location using avgCookieSale and random num cust/hr
+	totalCookies: numTotalCookies
+};
+
+var seaCenter = {
+	location: 'SeaTac Airport',
+	minCust: '11', // per hour
+	maxCust: '38', // per hour
+	avgCookieSale: '3.7', // per hour
+	// add method to generate random number of customers per hour
+	// calc and store amounts of cookies purchased for each hour at each location using avgCookieSale and random num cust/hr
+	totalCookies: numTotalCookies
+};
+
+var capHill = {
+	location: 'SeaTac Airport',
+	minCust: '20', // per hour
+	maxCust: '38', // per hour
+	avgCookieSale: '2.3', // per hour
+	// add method to generate random number of customers per hour
+	// calc and store amounts of cookies purchased for each hour at each location using avgCookieSale and random num cust/hr
+	totalCookies: numTotalCookies
+};
+
+var alki = {
+	location: 'SeaTac Airport',
+	minCust: '2', // per hour
+	maxCust: '16', // per hour
+	avgCookieSale: '4.6', // per hour
+	// add method to generate random number of customers per hour
+	// calc and store amounts of cookies purchased for each hour at each location using avgCookieSale and random num cust/hr
+	totalCookies: numTotalCookies
+
+};
